233ee1a60707488d8683eeced6f0fc24ec1fbf55
Emery Tate <tateejt@gmail.com>
2019-11-18 01:11:31 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthday_to_students.rb
+++ db/migrate/02_add_grade_and_birthday_to_students.rb
@@ -1,6 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :student, :grade, :in
+    add_column :student, :grade, :integer
+    add_column :students, :birthday, :string
     end
   end
 end
